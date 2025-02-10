3abb1eb80d72cf7acdc5a0013ce8cb05fb3db9b4
Alejandro241001 <alejandroangulomendez@outlook.com>
2025-02-10 10:56:26 +0100

    api-docs

--- src/main/java/org/iesalixar/daw2/Alejandroangulomendez/game_zone/config/SecurityConfig.java
+++ src/main/java/org/iesalixar/daw2/Alejandroangulomendez/game_zone/config/SecurityConfig.java
@@ -68,7 +68,7 @@ public class SecurityConfig {
                         ).hasRole("MANAGER") // Solo MANAGER
                         .requestMatchers(
                                 "/api/v1/authenticate",
-                                "/api/v1/register"
+                                "/api/v1/register", "/api/v3/api-docs"
                         ).permitAll() // Endpoints públicos
                         .anyRequest().authenticated() // El resto requiere autenticación
                 )
